5b0cab94b4ea55b4032b0cfb04e781e469544755
SahelAbraham <121370735+SahelAbraham@users.noreply.github.com>
2023-01-21 18:41:12 -0500

    First Commit

---
+++ EagleScoutProject/project.py
@@ -0,0 +1,8 @@
+# importing geopy library
+from geopy.geocoders import Nominatim
+
+coord = (26.8393, 80.9321)
+
+geolocator = Nominatim(user_agent='test/1')
+location = geolocator.reverse(coord)
+print(location.address) 
