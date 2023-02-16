5f84099e668d91a67cdaa607abecfc54608daefc
Chris J Abraham <ajsirch@yahoo.com>
2023-02-16 08:45:19 -0500

    Initial Commit for UI

---
+++ eagle_scout_project_ui/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {FileUploadPage} from './uploader/file_uploader'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <FileUploadPage/>
+    </div>
+  );
+}
+
+export default App;
