ca7e838b24a49b439f50a7b63399cc065c2e99da
Sahel Abraham <abrahamjsahel@gmail.com>
2023-04-13 00:05:49 -0400

    Finished Decoder for collecting jpg coordinates

--- EagleScoutProject/decoder.py
+++ EagleScoutProject/decoder.py
@@ -1,11 +1,10 @@
 import os
 
-image_list = os.listdir('c:\\Users\\abrah\\Desktop\\Project\\EagleScoutProject\\photos')
+image_list = os.listdir('c:\\Users\\abrah\\Desktop\\Project\\photos')
 image_list = [a for a in image_list if a.endswith('jpg')]
-
 
 from GPSPhoto import gpsphoto
 for a in image_list:
-    data = gpsphoto.getGPSData(os.getcwd() + f'\\{a}')
+    data = gpsphoto.getGPSData(os.getcwd() + '\\photos' + f'\\{a}')
     print(data)
 
