b336ca692e795f967804a935d2130ba8c1a7d5eb
Sahel Abraham <abrahamjsahel@gmail.com>
2023-04-12 22:41:15 -0400

    Added decoder to decode geolocation from jpgs

---
+++ EagleScoutProject/decoder.py
@@ -0,0 +1,11 @@
+import os
+
+image_list = os.listdir('c:\\Users\\abrah\\Desktop\\Project\\EagleScoutProject\\photos')
+image_list = [a for a in image_list if a.endswith('jpg')]
+
+
+from GPSPhoto import gpsphoto
+for a in image_list:
+    data = gpsphoto.getGPSData(os.getcwd() + f'\\{a}')
+    print(data)
+
