084284ae52c2147f40c497adc9fef5c93490b68c
Sahel Abraham <abrahamjsahel@gmail.com>
2023-04-14 16:26:52 -0400

    Fixed connection to MongoDB database, is setup now

--- EagleScoutProject/MongoServer.py
+++ EagleScoutProject/MongoServer.py
@@ -2,26 +2,13 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 
-uri = "mongodb+srv://EagleScout:<KHCRSTP1ZAI2KcWS>@dpwcluster.fkjzh59.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://EagleScout2:S2nmibuw@dpwcluster.fkjzh59.mongodb.net/?retryWrites=true&w=majority"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
-
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
-
-import datetime
-
-Document = {
-  "name": { "first": "Alan", "last": "Turing" },
-  "birth": datetime.datetime(1912, 6, 23),
-  "death": datetime.datetime(1954, 6, 7),
-  "contribs": [ "Turing machine", "Turing test", "Turingery" ],
-  "views": 1250000
-}
-
-Abraham.insert_one(Document)
