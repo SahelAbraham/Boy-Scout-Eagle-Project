52019f37912fa87ccd198920dd566c7b2831f7d7
Sahel Abraham <abrahamjsahel@gmail.com>
2023-06-24 19:05:35 -0400

    google maps api program can now interact with .csv

--- EagleScoutProject/project.py
+++ EagleScoutProject/project.py
@@ -1,12 +1,24 @@
 import gmplot
+import csv
 
-latitude_list = [ 40.5939636, 40.5940636, 30.3216419 ]
-longitude_list = [ -74.6049061, -74.6049061, 78.0413095 ]
+latitude_list = []
+longitude_list = []
+
+with open('latitudes.csv', newline = '') as f:
+    for row in csv.reader(f):
+        latitude_list.append(float(row[0]))
+
+with open('longitudes.csv', newline= '') as f:
+    for row in csv.reader(f):
+        longitude_list.append(float(row[0]))
+
+print(latitude_list)
+print(longitude_list)
 
 gmap1 = gmplot.GoogleMapPlotter.from_geocode("Bridgewater, New Jersey, United States", 15, apikey = 'AIzaSyALPKrX7n5tLMsi0M74X4QY8ppDIEIeSYM')
 #gmplot.GoogleMapPlotter(30.3164945, 78.03219179999999, 12, apikey= 'AIzaSyALPKrX7n5tLMsi0M74X4QY8ppDIEIeSYM')
 
 gmap1.scatter( latitude_list, longitude_list, '#FF0000', size = 1, marker = True )
 
-gmap1.draw("C:\\Users\\abrah\\Desktop\\map11.html")
+gmap1.draw("C:\\Users\\abrah\\Desktop\\map.html")
 
