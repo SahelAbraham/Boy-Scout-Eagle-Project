ed66453d57dca13ccba26171052b39876233f7c7
Sahel Abraham <abrahamjsahel@gmail.com>
2023-07-03 14:54:51 -0400

    uses folium map and markers (test)

--- EagleScoutProject/streamlitapp.py
+++ EagleScoutProject/streamlitapp.py
@@ -3,6 +3,7 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import folium
 from streamlit_folium import st_folium
+import pandas as pd
 
 
 uri = "mongodb+srv://EagleScout2:KEeIYE07Bj62U0KY@dpwcluster.fkjzh59.mongodb.net/?retryWrites=true&w=majority"
@@ -20,20 +21,45 @@ def getCoords(collection):
 StopSigns = getCoords('database.Stop_Signs')
 StormwaterDrains = getCoords('database.StormwaterDrains')
 
+#try:
+    #StopSigns = pd.DataFrame(getCoords('database.Stop_Signs'), columns = ['latitudes', 'longitudes'])
+#except:
+    #StopSigns = pd.DataFrame(columns = ['latitudes', 'longitudes'])
+
+#try:
+    #StormwaterDrains = pd.DataFrame(getCoords('database.StormwaterDrains'), columns = ['latitudes', 'longitudes'])
+#except:
+    #StormwaterDrains = pd.DataFrame(columns = ['latitudes', 'longitudes'])
+
+#print(StopSigns)
+#print(StormwaterDrains)
+
+StopSignPoints = pd.DataFrame({
+    'lon': StopSigns[1],
+    'lat': StopSigns[0]
+}, dtype=str)
+
+print(StopSignPoints)
 client.close()
 
 APP_TITLE = 'Infrastructure Map'
 APP_SUB_TITLE = 'For the Bridgewater Township'
 
 
-@st.cache_data(experimental_allow_widgets=True)
 def display_map():
     map = folium.Map(
         location = [40.5940, -74.6049],
-        zoom_start = 14,
-        scrollWheelZoom = False,
+        zoom_start = 13,
         tiles= 'cartodbpositron'
     )
+    for i in range(0, len(StopSignPoints)):
+        folium.CircleMarker(
+            location = [StopSignPoints.iloc[i]['lat'], StopSignPoints.iloc[i]['lon']],
+            radius = 2,
+            color = 'red',
+            tooltip = 'Stop Sign',
+            popup = ('Stop Sign' + '\nlatitude:' + StopSignPoints.iloc[i]['lat'] + '\nlongitude:' + StopSignPoints.iloc[i]['lon'])
+        ).add_to(map)
     st_map = st_folium(
         fig = map,
         height = 700,
